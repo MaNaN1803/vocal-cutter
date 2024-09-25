52bedfd66ae7b120c3a60650a3e9f2695e96b9fb
Manan Telrandhe <telrandhemanan@gmail.com>
2024-09-25 10:08:16 +0530

    mantine with app and index merged

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,18 @@
-import "@/styles/globals.css";
+// pages/_app.js
+import { MantineProvider, ColorSchemeProvider, ColorScheme } from '@mantine/core';
+import { useState } from 'react';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps }) {
+  const [colorScheme, setColorScheme] = useState('light');
+
+  const toggleColorScheme = (value) =>
+    setColorScheme((prev) => (prev === 'dark' ? 'light' : 'dark'));
+
+  return (
+    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+      <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
+        <Component {...pageProps} />
+      </MantineProvider>
+    </ColorSchemeProvider>
+  );
 }
